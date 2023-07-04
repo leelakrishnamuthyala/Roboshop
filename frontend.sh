42009b52d2ba26bb8456e8cf5a781f68bc6e0650
leelakrishna <131665617+leelakrishnamuthyala@users.noreply.github.com>
2023-07-04 11:05:31 +0530

    create frontend script and configure the files

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,6 @@ curl -o /tmp/frontend.zip https://roboshop.artifacts.s3.amazon.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-#file was missing
+cp roboshop.config /etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx
